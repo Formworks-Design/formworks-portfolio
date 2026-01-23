63af532564308f9384499ec6f0f2e689389107d8
Eric <eric@formworks.ltd>
2026-01-23 15:08:27 -0800

    Initial commit - Formworks portfolio

---
+++ projects.js
@@ -0,0 +1,52 @@
+// Add your projects here - just add the filename(s), title, and category
+// Images go in: images/projects/
+// For multiple images, use an array: images: ["img1.jpg", "img2.jpg"]
+
+const projects = [
+    {
+        images: ["20230604_125534.jpg"],
+        title: "Custom Logo Coin",
+        category: "Design & Manufacture"
+    },
+    {
+        images: ["20231011_123820.jpg", "20231011_123825.jpg"],
+        title: "Plasma Nozzle Organizer",
+        category: "Design & Manufacture"
+    },
+    {
+        images: ["20240208_085008.jpg"],
+        title: "Go/No-Go Inspection Gauge",
+        category: "Design & Manufacture"
+    },
+    {
+        images: ["20250916_125147.jpg"],
+        title: "Light Bezel (PA12-GF)",
+        category: "Design & Manufacture"
+    },
+    {
+        images: ["20251121_140300.jpg"],
+        title: "Parking Brake Switch Bezel",
+        category: "Design & Manufacture",
+        aspectRatio: "1/1"
+    },
+    {
+        images: ["20_3_PIECE_BARREL_BLANK_2026-Jan-12_09-31-32PM-000_CustomizedView22397456175.png"],
+        title: "3-Piece Truck Wheel",
+        category: "Wheel Design"
+    },
+    {
+        images: ["20_3_PIECE_RAPTOR_WHEELS_2026-Jan-13_12-47-39AM-000_CustomizedView44103972667.png"],
+        title: "3-Piece Raptor Wheel",
+        category: "Wheel Design"
+    },
+    {
+        images: ["dc1beab8-d74c-4746-8247-3c88f001f756.PNG"],
+        title: "Multi-Spoke 3-Piece Wheel",
+        category: "Wheel Design"
+    },
+    {
+        images: ["wheel_test_2026-Jan-12_06-16-46PM-000_CustomizedView26964999497.png"],
+        title: "Monoblock Spoked Wheel",
+        category: "Wheel Design"
+    }
+];
